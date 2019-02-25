55784807631e9f489cd4d46885cc19cefee6bb1c
jcraig203 <jcraig203@caledonian.ac.uk>
2019-02-25 21:58:53 +0000

    commit

---
+++ SecureSoftwareDevelopment/Controllers/FileUploadsController.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SecureSoftwareDevelopment.Models;
+
+namespace SecureSoftwareDevelopment.Controllers
+{
+    public class FileUploadsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: FileUploads
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View(db.FileUploads.ToList());
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        // GET: FileUploads/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FileUpload fileUpload = db.FileUploads.Find(id);
+            if (fileUpload == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fileUpload);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        // GET: FileUploads/Create
+        public ActionResult Create()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        // POST: FileUploads/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "fileID,SetFileName,expectedSizeofFile,documentType,docLocation")] FileUpload fileUpload)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (ModelState.IsValid)
+            {
+                db.FileUploads.Add(fileUpload);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(fileUpload);
+        }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+    }
+}
+
+        // GET: FileUploads/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FileUpload fileUpload = db.FileUploads.Find(id);
+            if (fileUpload == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fileUpload);
+        }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+    }
+}
+
+        // POST: FileUploads/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "fileID,SetFileName,expectedSizeofFile,documentType,docLocation")] FileUpload fileUpload)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (ModelState.IsValid)
+            {
+                db.Entry(fileUpload).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fileUpload);
+        }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+    }
+}
+
+        // GET: FileUploads/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FileUpload fileUpload = db.FileUploads.Find(id);
+            if (fileUpload == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fileUpload);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        // POST: FileUploads/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                FileUpload fileUpload = db.FileUploads.Find(id);
+            db.FileUploads.Remove(fileUpload);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+    }
+}
+        // GET: FileUploads/CopyTo/5
+        public ActionResult CopyTo(int? id)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                FileUpload fileUpload = db.FileUploads.Find(id);
+                if (fileUpload == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(fileUpload);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        // POST: FileUploads/CopyTo/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CopyTo([Bind(Include = "fileID,SetFileName,expectedSizeofFile,documentType,docLocation")] FileUpload fileUpload)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (ModelState.IsValid)
+                {
+                    db.FileUploads.Add(fileUpload);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View(fileUpload);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SecureSoftwareDevelopment/Models/FileUpload.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SecureSoftwareDevelopment.Models
+{
+    public class FileUpload
+    {
+        [ScaffoldColumn(false)]
+        [Key]
+        public int fileID { get; set; }
+
+        [DisplayName("Set the file name")]
+        [Required(ErrorMessage = "A file name is required")]
+        [StringLength(15)]
+        public string SetFileName { get; set; }
+
+        [DisplayName("Set the file expected size in MB")]
+        [Required(ErrorMessage = "Please ensure numbers only are used")]
+        [Range(0.01, 600.00, ErrorMessage = "Price must be between 0.01 and 600.00")]
+        public int expectedSizeofFile { get; set; }
+
+        [DisplayName("Set the file type, e.g. Doc, Exl")]
+        [Required(ErrorMessage = "A file type is required")]
+        [StringLength(10)]
+        public string documentType { get; set; }
+
+        [DisplayName("Set location")]
+        [Required(ErrorMessage = "A location is required")]
+        [StringLength(15)]
+        public string docLocation { get; set; }
+
+    }
+}
